fb970b7082107f038e98f0fcbf98c5e519ec6f4c
greyhoundIlya <bobicp01@gmail.com>
2025-10-24 14:17:43 +0200

    react-dymamic-v2

--- src/App.tsx
+++ src/App.tsx
@@ -11,34 +11,30 @@ import { get5First, getAll, getRedGoods } from './api/goods';
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
-  useEffect(() => {
-    getRedGoods().then(setGoods);
-  }, []);
-
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
       <button
-      onClick={() => getAll().then(setGoods)}
-      type="button"
-      data-cy="all-button"
+        onClick={() => getAll().then(setGoods)}
+        type="button"
+        data-cy="all-button"
       >
         Load all goods
       </button>
 
       <button
-      type="button"
-      data-cy="first-five-button"
-      onClick={() => get5First().then(setGoods)}
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => get5First().then(setGoods)}
       >
         Load 5 first goods
       </button>
 
       <button
-      type="button"
-      data-cy="red-button"
-      onClick={() => getRedGoods().then(setGoods)}
+        type="button"
+        data-cy="red-button"
+        onClick={() => getRedGoods().then(setGoods)}
       >
         Load red goods
       </button>
